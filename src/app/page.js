c40d41665aaa6ba1829bc23ce9c60588c065dcea
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-10-27 15:38:47 +0600

    fix delete task bug

--- src/app/page.js
+++ src/app/page.js
@@ -45,13 +45,16 @@ const Home = () => {
 
   const handleDeleteTodo = async (id) => {
     try {
-      const res = await fetch("http://localhost:3000/api/todo/delete-todo", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id }),
-      });
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/todo/delete-todo`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id }),
+        }
+      );
 
       const data = await res.json();
       if (res.status === 200) {
